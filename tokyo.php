5987bc70d1e12beff70ae662a82dc26b87857030
unknown <jeremiahsimo26@gmail.com>
2024-11-29 17:29:53 +0800

    uploaded modular method example code

---
+++ tokyo.php
@@ -0,0 +1,6 @@
+
+    <h1>tokyo</h1>
+    <p>tokyo the land of otaku</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+ 
+ 
