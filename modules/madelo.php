e762a0e2201a9c8094a62abf2a3728d56508cd75
unknown <arda.madelo.coc@phinmaed.com>
2024-11-29 18:30:52 +0800

    added new

---
+++ modules/madelo.php
@@ -0,0 +1,4 @@
+<p><strong>Name:</strong> Carl Vinncent D. Madelo</p>
+    <p><strong>Age:</strong> 21</p>
+    <p><strong>Occupation:</strong> Software Developer</p>
+    <p><strong>Address:</strong>Zone 9, Macanhan, Carmen, CDO</p>
