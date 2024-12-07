65314a7562fbb72dcb47e100c608a8c9eb629844
unknown <sumandaellevera@gmail.com>
2024-12-07 17:57:59 +0800

    update

--- home_page.php
+++ home_page.php
@@ -100,15 +100,15 @@ footer {
 
             switch ($page){
                 case 'london':
-                    include 'london.php';
+                    include 'modules/london.php';
                     break;
                     
                     case 'paris':
-                        include 'paris.php';
+                        include 'modules/paris.php';
                         break;
                         
                         case 'tokyo':
-                            include 'tokyo.php';
+                            include 'modules/tokyo.php';
                             break;
 
                             case 'My Profile':
