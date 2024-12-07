fbf0b88b0465d3453291c1a7224beb5c7d323427
unknown <cara.hibaya.coc@phinmaed.com>
2024-12-07 17:55:37 +0800

    hibaya added

--- modules/Hibaya.php
+++ modules/Hibaya.php
@@ -1,46 +1,6 @@
-<section>
-  <nav>
-    <ul>
-      <li><a href="home_page.php?page=london">London</a></li>
-      <li><a href="home_page.php?page=paris">Paris</a></li>
-      <li><a href="home_page.php?page=tokyo">Tokyo</a></li>
-      <li><a href="home_page.php?page=hibaya">hibaya</a></li>
-      
-    </ul>
-  </nav>
-  
-  <article>
-   <!-- content area -->
-    <?php
-        if (isset($_GET['page'])){
-            $page=$_GET['page'];
-
-            switch ($page){
-                case 'london':
-                    include 'modules/london.php';
-                    break;
-                    
-                    case 'paris':
-                        include 'modules/paris.php';
-                        break;
-                        
-                        case 'tokyo':
-                            include 'modules/tokyo.php';
-                            break;
-
-                            case 'hibaya':
-                              include 'modules/hibaya.php';
-                              break;
-
-            }
-
-        }
-    ?>
-  </article>
-</section>
-
-
 <h1>Carl Jeffrey Hibaya</h1>
+    <p>
     <p>Student/gwapo/crew</p>
+    <p>
     <p>macanhan, carmen, cagaayan de oro city</p>
  
