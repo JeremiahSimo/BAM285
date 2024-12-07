f539e8663770a6a633a1f945251916fbc998cb09
Zhedrix <mangondatozhedrick@gmail.com>
2024-12-07 18:12:03 +0800

    this is my file

--- home_page.php
+++ home_page.php
@@ -87,6 +87,7 @@ footer {
       <li><a href="home_page.php?page=london">London</a></li>
       <li><a href="home_page.php?page=paris">Paris</a></li>
       <li><a href="home_page.php?page=tokyo">Tokyo</a></li>
+      <li><a href="home_page.php?page=mangondato">mangondato</a></li>
     </ul>
   </nav>
   
@@ -108,6 +109,10 @@ footer {
                         case 'tokyo':
                             include 'tokyo.php';
                             break;
+
+                            case 'mangondato':
+                              include 'mangondato.php';
+                              break;
 
             }
 
---
+++ modules/mangondato.php
@@ -0,0 +1,5 @@
+<h1> Zhedrick Mangondato <h1>
+
+<p> 3rd year BSIT <P>
+   <p> PHINMA COC </P>
+    
