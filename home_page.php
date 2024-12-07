39013ba6c20738de492374d998c4779a8b931e6c
unknown <cara.hibaya.coc@phinmaed.com>
2024-12-07 17:53:43 +0800

    hibaya added

--- home_page.php
+++ home_page.php
@@ -100,19 +100,19 @@ footer {
 
             switch ($page){
                 case 'london':
-                    include 'london.php';
+                    include 'modules/london.php';
                     break;
                     
                     case 'paris':
-                        include 'paris.php';
+                        include 'modules/paris.php';
                         break;
                         
                         case 'tokyo':
-                            include 'tokyo.php';
+                            include 'modules/tokyo.php';
                             break;
 
                             case 'hibaya':
-                              include 'hibaya.php';
+                              include 'modules/hibaya.php';
                               break;
 
             }
