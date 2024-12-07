129e33c8a8276ecda906c810f4448849b52b571b
unknown <undrawpinakagwapo@gmail.com>
2024-12-07 17:59:59 +0800

    sirr

--- home_page.php
+++ home_page.php
@@ -101,19 +101,19 @@ footer {
 
             switch ($page){
                 case 'london':
-                    include 'london.php';
+                    include 'module/london.php';
                     break;
                     
                     case 'paris':
-                        include 'paris.php';
+                        include 'module/paris.php';
                         break;
                         
                         case 'tokyo':
-                            include 'tokyo.php';
+                            include 'module/tokyo.php';
                             break;
 
                             case 'andrea':
-                              include 'andrea.php';
+                              include 'modules/andrea.php';
                               break;
 
             }
