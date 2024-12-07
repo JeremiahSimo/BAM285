1bb6aeb5099ccb6a71dc06f05faacb5adcbe4f40
unknown <cley.taroy.coc@phinmaed.com>
2024-12-07 17:42:35 +0800

    my personal info

--- modules/taroy.php
+++ modules/taroy.php
@@ -83,7 +83,7 @@ footer {
       <li><a href="home_page.php?page=london">London</a></li>
       <li><a href="home_page.php?page=paris">Paris</a></li>
       <li><a href="home_page.php?page=tokyo">Tokyo</a></li>
-      <li><a href="home_page.php?page=My personal info"></a></li>
+      <li><a href="home_page.php?page=My personal info">My personal info</a></li>
     </ul>
   </nav>
 
