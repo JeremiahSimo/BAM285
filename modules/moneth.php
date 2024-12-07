695326bbd0f67497ae3d07232f3be93a2c5209ba
unknown <beamoneth000@gmail.com>
2024-12-07 18:19:28 +0800

    sir

--- modules/moneth.php
+++ modules/moneth.php
@@ -1,6 +1,12 @@
-echo "<h1> moneth<h1> "
-
-echo "<p> 3rd year BSIT<p>"
-echo "<p> Section 2</p>"
-echo "<p>09061670642</p>"
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction</title>
+</head>
+<body>
+    <h1>My Name is Moneth Mag Pulong</h1>
+    <p>I am 100 years old.</p>
+</body>
+</html>
