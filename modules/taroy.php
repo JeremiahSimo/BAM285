da403e093c8757caa363a6b76985c795c408849d
unknown <cley.taroy.coc@phinmaed.com>
2024-12-07 18:43:22 +0800

    yes

--- modules/taroy.php
+++ modules/taroy.php
@@ -1,6 +1,7 @@
 
 <H1>CLINE JAY TAROY</H1>
 <p>BSIT 3RD</p>
-<P>Taga Cogon Gitagum Purok 3 way laing gwapo nga</P>
+<P>Taga Cogon Gitagum Purok 3</P>
+<p>way laing gwapo nga</p>
 <p>lumulopyo didto</p>
-
+<link rel="facebook" href="https://www.facebook.com/cline.jay.2024/">
