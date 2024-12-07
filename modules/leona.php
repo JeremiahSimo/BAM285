86dea7b6c95c93bb7ab49edf8254c736a37b8b8e
unknown <honeyfaithleona7@gmail.com>
2024-12-07 18:24:48 +0800

    leona

--- modules/leona.php
+++ modules/leona.php
@@ -1,16 +1,4 @@
-<div style="background-color: #fff; padding: 30px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); text-align: center; max-width: 400px; width: 100%;">
-        <h1 style="color: #5f6368; font-size: 24px;">Welcome!</h1>
-        
-        <?php
-           
-            $name = "Honey Faith Leona";
-            $year = 3; 
-            $course = "Information Technology";
-            $school = "PHINMA COC";
-
-
-            echo "<p style='color: #555; font-size: 18px; margin: 10px 0;'>Hello, I'm <span style='font-weight: bold; color: #00796b;'>$name</span>, a <span style='font-weight: bold; color: #00796b;'>$year</span>-year $course student at <span style='font-weight: bold; color: #00796b;'>$school</span>.</p>";
-            echo "<p>Email: honeyfaithleona </p> "
-            echo "<p>Number: 09261148360</p>"
-        ?>
-    </div>
+<h1>Honey Faith Leona</h1>
+<p>09261148360</p>
+<p>Email: Honeyfaithleona7@gmail.com</p>
+<p>Information technology</p>
