198d1509bfa34e5b7264e49d89e657b2ee7bccac
Janneth01062003 <kithenna.caamio@gmail.com>
2024-12-20 18:47:33 +0800

    attendance form

--- modules/attendance.php
+++ modules/attendance.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'school_management';
+$database = 'registration_db';
 
 
 $conn = new mysqli($host, $username, $password, $database);
